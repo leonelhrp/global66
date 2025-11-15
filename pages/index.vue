13be99dfe8fc68fcf0735e6c2b737224e97851ff
Leonel Rojas <leonelrojasperez@gmail.com>
2025-11-15 12:49:32 -0300

    feat(pages): add index page with redirect

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+/**
+ * Página de redirección
+ * Redirige automáticamente a /precio/peso-chileno como ruta por defecto
+ */
+
+export default {
+  name: 'HomePage',
+
+  /**
+   * Middleware para redirigir
+   * Se ejecuta antes de renderizar la página
+   */
+  asyncData({ redirect }) {
+    return redirect(301, '/precio/peso-chileno')
+  }
+}
+</script>
